a1513a3ce50dac3a72a093582aee3782a44286ab
Owen-E22 <owen.eigen@icloud.com>
2024-09-04 20:21:14 -0500

    fix

--- Proverbs22.py
+++ Proverbs22.py
@@ -1,5 +1,4 @@
-
 print('“Do you see a man skillful in his work?')
 print('He will stand before kings;')
 print('he will not stand before obscure men.”')
-print('      (Proverbs 22:29)')
+print("      (Proverbs 22:29)")
