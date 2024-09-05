75651c2e23cacc916326864afa4502ba7cd7d252
Owen-E22 <owen.eigen@icloud.com>
2024-09-04 19:43:37 -0500

    Proverbs 22:29

---
+++ Proverbs22.py
@@ -0,0 +1,6 @@
+Owen Eigen, 9/04/2024, Proverbs 22:29
+
+print('“Do you see a man skillful in his work?')
+print('He will stand before kings;')
+print('he will not stand before obscure men.”')
+print'      (Proverbs 22:29)')
